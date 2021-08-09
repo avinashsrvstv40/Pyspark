8ee7715d89f4528b76db1b9823ffa4374fffb366
avinashsrvstv40 <avinashsrvstv40@gmail.com>
2021-08-09 20:26:13 +0800

    Broadcasting Dictionary

--- Boradcast Variable/broadcast_variable.py
+++ Boradcast Variable/broadcast_variable.py
@@ -1,7 +1,8 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, initcap
 from pyspark.sql.types import IntegerType
-from lib.util import get_spark_config
+from lib.util import get_spark_config, load_employee_data
+import sys
 
 
 def state_convert(code):
@@ -14,11 +15,14 @@ if __name__ == "__main__":
     spark = SparkSession.builder. \
         config(conf=conf).getOrCreate()
 
+    if len(sys.argv) != 2:
+        raise Exception("Require data file")
+        # sys.exit(-1)
+
     states = {"NY": "New York", "CA": "California", "FL": "Florida"}
     broadcastStates = spark.sparkContext.broadcast(states)
 
-    employee_df = spark.read.option("header", "true") \
-        .csv("C:\\Users\\user\\Downloads\\5000-Records\\5000_Records.csv") \
+    employee_df = load_employee_data(spark, sys.argv[1]) \
         .withColumnRenamed("Age in Yrs.", "Age") \
         .withColumn("Age", col("Age").cast(IntegerType())) \
         .withColumn("First Name", initcap(col("First Name"))) \
@@ -28,10 +32,10 @@ if __name__ == "__main__":
     employee_df.show(truncate=False)
 
     # Broadcast variable on filter
-    filteDf = employee_df.where((employee_df['State'].isin(list(broadcastStates.value.keys()))))
+    filterDf = employee_df.where((employee_df['State'].isin(list(broadcastStates.value.keys()))))
 
-    filtered_df = filteDf.select("Emp ID", "First Name", "Last Name", "State") \
+    filtered_df = filterDf.select("Emp ID", "First Name", "Last Name", "State") \
         .rdd.map(lambda x: (x[0], x[1], x[2], state_convert(x[3])))
 
-    spark.createDataFrame(filtered_df, schema=["Emp ID", "Firs tName", "Last Name",
-                                               "State"]).show(False)
+    spark.createDataFrame(filtered_df, schema=["Emp ID", "First Name", "Last Name", "State"]) \
+        .show(truncate=False)
